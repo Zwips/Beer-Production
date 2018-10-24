f014362a3fcc1e104fba9150fb21cc3e9916e1a1
Maltesj <malte_sj@hotmail.com>
2018-10-24 12:49:13 +0200

    maven er rykket til ryggen

---
+++ Semester Project 3/src/machineConnection/SubscriptionCreator.java
@@ -0,0 +1,34 @@
+package machineConnection;
+
+import Acquantiance.IDataChangeCatcher;
+import com.prosysopc.ua.ServiceException;
+import com.prosysopc.ua.StatusException;
+import com.prosysopc.ua.client.MonitoredDataItem;
+import com.prosysopc.ua.client.MonitoredDataItemListener;
+import com.prosysopc.ua.client.Subscription;
+import com.prosysopc.ua.client.UaClient;
+import org.opcfoundation.ua.builtintypes.DataValue;
+import org.opcfoundation.ua.builtintypes.NodeId;
+import org.opcfoundation.ua.core.Attributes;
+import org.opcfoundation.ua.core.MonitoringMode;
+
+
+public class SubscriptionCreator {
+
+    static Subscription createSubscription(UaClient client, NodeId node, IDataChangeCatcher dataChangeCatcher) throws ServiceException, StatusException {
+        MonitoredDataItem monitoredDataItem = new MonitoredDataItem(node, Attributes.Value, MonitoringMode.Reporting);
+        Subscription subscription = new Subscription();
+
+        MonitoredDataItemListener dataItemListener = new MonitoredDataItemListener() {
+            @Override
+            public void onDataChange(MonitoredDataItem sender, DataValue prevValue, DataValue value) {
+              dataChangeCatcher.report(value);
+            }
+        };
+
+        client.addSubscription(subscription);
+        subscription.addItem(monitoredDataItem);
+        monitoredDataItem.setDataChangeListener(dataItemListener);
+        return subscription;
+    }
+}
