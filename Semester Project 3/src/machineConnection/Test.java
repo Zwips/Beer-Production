7e4f9b27e01960faa962d5002288497e906172e0
HCHB <32193248+HCHB@users.noreply.github.com>
2018-10-24 17:18:04 +0200

    Commit and "Fri???"

--- Semester Project 3/src/machineConnection/Test.java
+++ Semester Project 3/src/machineConnection/Test.java
@@ -1,4 +1,4 @@
-/*
+
 package machineConnection;
 
 import com.prosysopc.ua.ServiceException;
@@ -16,149 +16,184 @@ import static java.lang.Thread.sleep;
 
 public class Test {
 
-    public static void main(String[] args) throws InterruptedException {
+//<editor-fold desc="Raw machine start">
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        MachineConnection machineConnection = new MachineConnection();
+//
+//        UaClient client = machineConnection.getConnection("127.0.0.1:4840", "1234","sdu");
+//
+//        start(client);
+//    }
+//
+//    private static void start(UaClient client) throws InterruptedException {
+//
+//        reset(client);
+////        sleep(1000);
+//
+//        machSpeed(client);
+////        sleep(1000);
+//
+//        batchID(client);
+////        sleep(1000);
+//
+//        productID(client);
+////        sleep(1000);
+//
+//        amount(client);
+////        sleep(1000);
+//
+//        startMachine(client);
+////        sleep(1000);
+//    }
+//
+//    private static void amount(UaClient client) {
+//        String identifier = "Command.Parameter[2].Value";
+//        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
+//        DataValue dv1 = new DataValue(new Variant(1000F));
+//
+//        try {
+//            System.out.println("sent command to: " + identifier);
+//            System.out.println(client.writeValue(node, dv1));
+//            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
+//            System.out.println(client.readValue(node));
+//            System.out.println("**********************************");
+//            System.out.println("");
+//        } catch (ServiceException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (StatusException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    private static void productID(UaClient client) {
+//        String identifier = "Command.Parameter[1].Value";
+//        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
+//        DataValue dv1 = new DataValue(new Variant(3.0F));
+//
+//        try {
+//            System.out.println("sent command to: " + identifier);
+//            System.out.println(client.writeValue(node, dv1));
+//            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
+//            System.out.println(client.readValue(node));
+//            System.out.println("**********************************");
+//            System.out.println("");
+//        } catch (ServiceException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (StatusException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    private static void batchID(UaClient client) {
+//        String identifier = "Command.Parameter[0].Value";
+//        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
+//        DataValue dv1 = new DataValue(new Variant(new Float(50)));
+//
+//        try {
+//            System.out.println("sent command to: " + identifier);
+//            System.out.println(client.writeValue(node, dv1));
+//            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
+//            System.out.println(client.readValue(node));
+//            System.out.println("**********************************");
+//            System.out.println("");
+//        } catch (ServiceException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (StatusException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    private static void machSpeed(UaClient client) {
+//        String identifier = "Command.MachSpeed";
+//        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
+//        DataValue dv1 = new DataValue(new Variant(100F));
+//
+//        try {
+//            System.out.println("sent command to: " + identifier);
+//            System.out.println(client.writeValue(node, dv1));
+//            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
+//            System.out.println(client.readValue(node));
+//            System.out.println("**********************************");
+//            System.out.println("");
+//        } catch (ServiceException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (StatusException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    private static void startMachine(UaClient client) {
+//        String identifier = "Command.CntrlCmd";
+//        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
+//        DataValue dv1 = new DataValue(new Variant(2));
+//
+//        try {
+//            System.out.println("sent command to: " + identifier);
+//            System.out.println(client.writeValue(node, dv1));
+//            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
+//            System.out.println(client.readValue(node));
+//            System.out.println("**********************************");
+//            System.out.println("");
+//        } catch (ServiceException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (StatusException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    private static void reset(UaClient client) {
+//        String identifier = "Command.CntrlCmd";
+//        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
+//        DataValue dv1 = new DataValue(new Variant(1));
+//
+//        try {
+//            System.out.println("sent command to: " + identifier);
+//            System.out.println(client.writeValue(node, dv1));
+//            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
+//            System.out.println(client.readValue(node));
+//            System.out.println("**********************************");
+//            System.out.println("");
+//        } catch (ServiceException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (StatusException ex) {
+//            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//</editor-fold>
 
-        MachineConnection machineConnection = new MachineConnection();
+    private static MachineConnection connection = new MachineConnection("127.0.0.1:4840", "sdu","1234");
 
-        UaClient client = machineConnection.getConnection("127.0.0.1:4840", "1234","sdu");
+    public static void main(String[] args) throws ServiceException, StatusException, InterruptedException {
+        sleep(100);
 
-        start(client);
+        connection.setControlCommand(1);
+        sleep(100);
+
+        connection.setProductIDForNextBatch(0);
+        sleep(100);
+
+        connection.setAmountInNextBatch(60000);
+        sleep(100);
+
+        connection.setBatchIDForNextBatch(600);
+        sleep(100);
+
+        connection.setMachineSpeed(600);
+        sleep(100);
+
+        connection.setControlCommand(2);
+
+
+        connection.subscribeToCurrentState(new SubscribtiontestState(new Test()));
     }
 
-    private static void start(UaClient client) throws InterruptedException {
 
-        reset(client);
-//        sleep(1000);
+    public void printProduction() throws ServiceException, StatusException, InterruptedException {
 
-        machSpeed(client);
-//        sleep(1000);
-
-        batchID(client);
-//        sleep(1000);
-
-        productID(client);
-//        sleep(1000);
-
-        amount(client);
-//        sleep(1000);
-
-        startMachine(client);
-//        sleep(1000);
+        System.out.println("Number of produced products "+connection.readNumberOfProducedProducts());
+        sleep(100);
+        System.out.println("Number of defective products "+connection.readNumberOfDefectiveProducts());
     }
-
-    private static void amount(UaClient client) {
-        String identifier = "Command.Parameter[2].Value";
-        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
-        DataValue dv1 = new DataValue(new Variant(1000F));
-
-        try {
-            System.out.println("sent command to: " + identifier);
-            System.out.println(client.writeValue(node, dv1));
-            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
-            System.out.println(client.readValue(node));
-            System.out.println("**********************************");
-            System.out.println("");
-        } catch (ServiceException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (StatusException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static void productID(UaClient client) {
-        String identifier = "Command.Parameter[1].Value";
-        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
-        DataValue dv1 = new DataValue(new Variant(3.0F));
-
-        try {
-            System.out.println("sent command to: " + identifier);
-            System.out.println(client.writeValue(node, dv1));
-            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
-            System.out.println(client.readValue(node));
-            System.out.println("**********************************");
-            System.out.println("");
-        } catch (ServiceException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (StatusException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static void batchID(UaClient client) {
-        String identifier = "Command.Parameter[0].Value";
-        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
-        DataValue dv1 = new DataValue(new Variant(new Float(50)));
-
-        try {
-            System.out.println("sent command to: " + identifier);
-            System.out.println(client.writeValue(node, dv1));
-            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
-            System.out.println(client.readValue(node));
-            System.out.println("**********************************");
-            System.out.println("");
-        } catch (ServiceException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (StatusException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static void machSpeed(UaClient client) {
-        String identifier = "Command.MachSpeed";
-        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
-        DataValue dv1 = new DataValue(new Variant(100F));
-
-        try {
-            System.out.println("sent command to: " + identifier);
-            System.out.println(client.writeValue(node, dv1));
-            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
-            System.out.println(client.readValue(node));
-            System.out.println("**********************************");
-            System.out.println("");
-        } catch (ServiceException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (StatusException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static void startMachine(UaClient client) {
-        String identifier = "Command.CntrlCmd";
-        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
-        DataValue dv1 = new DataValue(new Variant(2));
-
-        try {
-            System.out.println("sent command to: " + identifier);
-            System.out.println(client.writeValue(node, dv1));
-            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
-            System.out.println(client.readValue(node));
-            System.out.println("**********************************");
-            System.out.println("");
-        } catch (ServiceException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (StatusException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static void reset(UaClient client) {
-        String identifier = "Command.CntrlCmd";
-        NodeId node = new NodeId(6, "::Program:Cube." + identifier);
-        DataValue dv1 = new DataValue(new Variant(1));
-
-        try {
-            System.out.println("sent command to: " + identifier);
-            System.out.println(client.writeValue(node, dv1));
-            client.writeValue(new NodeId(6, "::Program:Cube.Command.CmdChangeRequest"), new DataValue(new Variant(true)));
-            System.out.println(client.readValue(node));
-            System.out.println("**********************************");
-            System.out.println("");
-        } catch (ServiceException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (StatusException ex) {
-            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
 }
-*/
