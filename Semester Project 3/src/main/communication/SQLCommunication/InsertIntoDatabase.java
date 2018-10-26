cff31b997003c142858e5b6b8b331b3ccddc7ed3
Zwips <32239749+Zwips@users.noreply.github.com>
2018-10-26 13:04:15 +0200

    Insert and Select statements

--- Semester Project 3/src/main/communication/SQLCommunication/InsertIntoDatabase.java
+++ Semester Project 3/src/main/communication/SQLCommunication/InsertIntoDatabase.java
@@ -10,17 +10,18 @@ public class InsertIntoDatabase {
 
 	DatabaseHandler dbHandler;
 
-	public void InsertIntoBatch(int amount, int batchID, String types, int defective) {
+	public void InsertIntoBatch(int batchID, String productType, int amount, int defective) {
 
 		Statement st = null;
 		ResultSet rs = null;
 
 		try{
 			st = dbHandler.OpenConnection();
-			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Batch(Amount, BatchID, Types, Defective) VALUES (?, ?, ?, ?)");
-			pStatement.setInt(1, amount);
-			pStatement.setInt(2, batchID);
-			pStatement.setString(3, types);
+			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Batch(BatchID, ProductType, Amount, Defective) VALUES (?, ?, ?, ?)");
+
+			pStatement.setInt(1, batchID);
+			pStatement.setString(2, productType);
+			pStatement.setInt(3, amount);
 			pStatement.setInt(4, defective);
 			rs = pStatement.executeQuery();
 		} catch(SQLException e){
@@ -38,7 +39,7 @@ public class InsertIntoDatabase {
 		}
 	}
 
-	public void InsertIntoBatch_log(int BatchID, int MachineID) {
+	public void InsertIntoBatch_log(int batchID, int MachineID) {
 
 		Statement st = null;
 		ResultSet rs = null;
@@ -46,7 +47,7 @@ public class InsertIntoDatabase {
 		try {
 			st = dbHandler.OpenConnection();
 			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Batch_log(BatchID, MachineID) VALUES (?, ?)");
-			pStatement.setInt(1, BatchID);
+			pStatement.setInt(1, batchID);
 			pStatement.setInt(2, MachineID);
 			rs = pStatement.executeQuery();
 		} catch (SQLException e) {
@@ -65,16 +66,17 @@ public class InsertIntoDatabase {
 
 	}
 
-	public void InsertIntoHumidity(int BatchID, String timeOfReading) {
+	public void InsertIntoHumidity(int batchID, String timeOfReading, float valuePercent) {
 
 		Statement st = null;
 		ResultSet rs = null;
 
 		try {
 			st = dbHandler.OpenConnection();
-			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Humidity(BatchID, timeOfReading) VALUES (?, ?)");
-			pStatement.setInt(1, BatchID);
+			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Humidity(BatchID, timeOfReading, ValuePercent) VALUES (?, ?, ?)");
+			pStatement.setInt(1, batchID);
 			pStatement.setString(2, timeOfReading);
+			pStatement.setFloat(3, valuePercent);
 			rs = pStatement.executeQuery();
 		} catch (SQLException e) {
 			System.out.println("Exception" + e);
@@ -92,16 +94,17 @@ public class InsertIntoDatabase {
 
 	}
 
-	public void InsertIntoTemperature(int BatchID, String timeOfReading) {
+	public void InsertIntoTemperature(int batchID, String timeOfReading, float valueCelcius) {
 
 		Statement st = null;
 		ResultSet rs = null;
 
 		try {
 			st = dbHandler.OpenConnection();
-			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Humidity(BatchID, timeOfReading) VALUES (?, ?)");
-			pStatement.setInt(1, BatchID);
+			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Temperature(BatchID, timeOfReading, ValueCelcius) VALUES (?, ?, ?)");
+			pStatement.setInt(1, batchID);
 			pStatement.setString(2, timeOfReading);
+			pStatement.setFloat(3, valueCelcius);
 			rs = pStatement.executeQuery();
 		} catch (SQLException e) {
 			System.out.println("Exception" + e);
@@ -119,16 +122,17 @@ public class InsertIntoDatabase {
 
 	}
 
-	public void InsertIntoVibration(int BatchID, String timeOfReading) {
+	public void InsertIntoVibration(int batchID, String timeOfReading, float valuePBS) {
 
 		Statement st = null;
 		ResultSet rs = null;
 
 		try {
 			st = dbHandler.OpenConnection();
-			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Vibration(BatchID, timeOfReading) VALUES (?, ?)");
-			pStatement.setInt(1, BatchID);
+			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Vibration(BatchID, timeOfReading, ValuePBS) VALUES (?, ?, ?)");
+			pStatement.setInt(1, batchID);
 			pStatement.setString(2, timeOfReading);
+			pStatement.setFloat(3, valuePBS);
 			rs = pStatement.executeQuery();
 		} catch (SQLException e) {
 			System.out.println("Exception" + e);
@@ -144,6 +148,38 @@ public class InsertIntoDatabase {
 			}
 		}
 
+	}
+
+	public void InsertIntoOrders(int amount, String productType, String earliestDeliveryDate, String latestDeliveryDate, int priority, boolean status, int batchID) {
+
+		Statement st = null;
+		ResultSet rs = null;
+
+		try{
+			st = dbHandler.OpenConnection();
+			PreparedStatement pStatement = st.getConnection().prepareStatement("INSERT INTO Orders(Amount, ProductType, EarliestDeliveryDate, LatestDeliveryDate, Priority, Status, BatchID) VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+			pStatement.setInt(1, amount);
+			pStatement.setString(2, productType);
+			pStatement.setString(3, earliestDeliveryDate);
+			pStatement.setString(4, latestDeliveryDate);
+			pStatement.setInt(5, priority);
+			pStatement.setBoolean(6, status);
+			pStatement.setInt(7, batchID);
+			rs = pStatement.executeQuery();
+		} catch(SQLException e){
+			System.out.println("Exception" + e);
+		} finally{
+			try{
+				st.getConnection().close();
+				st.close();
+				if (rs!=null){
+					rs.close();
+				}
+			}catch(SQLException e) {
+
+			}
+		}
 	}
 
 }
