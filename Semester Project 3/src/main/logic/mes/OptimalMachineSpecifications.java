40cb765ab43638f8e1761138e7c58f2214b38ad5
Maltesj <malte_sj@hotmail.com>
2018-10-26 12:12:27 +0200

    Alt er godt :D

---
+++ Semester Project 3/src/main/logic/mes/OptimalMachineSpecifications.java
@@ -0,0 +1,30 @@
+package logic.mes;
+
+public class OptimalMachineSpecifications {
+
+
+    float getOptimalSpeed(int productType){
+        switch (productType) {
+            case 0:
+                return 600;
+
+            case 1:
+                return 300;
+
+            case 2:
+                return 150;
+
+            case 3:
+                return 200;
+
+            case 4:
+                return 100;
+
+            case 5:
+                return 125;
+
+            default:
+                return 100;
+        }
+    }
+}
