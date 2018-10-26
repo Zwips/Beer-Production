a99137b020d29eb77d0b541a9580f0d509b2bc63
HCHB <32193248+HCHB@users.noreply.github.com>
2018-10-26 13:56:48 +0200

    Beginning of BDD

--- Semester Project 3/src/test/java/virtualSimulation/ConnectSimulation.java
+++ Semester Project 3/src/test/java/virtualSimulation/ConnectSimulation.java
@@ -1,6 +1,7 @@
-package stepdefs;
+package virtualSimulation;
 
 import communication.machineConnection.MachineConnection;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -15,13 +16,12 @@ import static org.junit.Assert.assertEquals;
 public class ConnectSimulation {
 
     private MachineConnection connection;
+    private Float temperature;
+    private Float vibration;
+    private Float humidity;
 
-    @SuppressWarnings("Duplicates")
     @Given("^The simulation is started$")
     public void theSimulationIsStarted() {
-
-        // This can't be done from Java??
-        // Runtime.getRuntime().exec("cmd.exe", "/c", ".");
         File file = new File("Simulation/start.bat");
         Process process;
 
@@ -43,22 +43,18 @@ public class ConnectSimulation {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 
     @When("^connecting the the virtual simulation$")
     public void connectingTheTheVirtualSimulation() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         this.connection = new MachineConnection("127.0.0.1:4840","sdu","1234");
-
     }
 
-    @Then("^The machine connection is connected$")
-    public void theMachineConnectionIsConnected() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
+    @Then("^The machine connection is connected to the virtual simulation$")
+    public void theMachineConnectionIsConnectedToTheVirtualSimulation() throws Throwable {
         boolean connected = connection.isConnected();
 
         assertEquals(true,connected);
     }
+
 }
