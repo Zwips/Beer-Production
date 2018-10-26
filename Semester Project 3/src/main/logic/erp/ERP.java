67166b9e30adcda5139c2fb675cfa6f5aed5d0c7
michaeltrier1 <32192126+michaeltrier1@users.noreply.github.com>
2018-10-26 11:04:14 +0200

    1

--- Semester Project 3/src/main/logic/erp/ERP.java
+++ Semester Project 3/src/main/logic/erp/ERP.java
@@ -1,5 +1,6 @@
 package logic.erp;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -7,9 +8,10 @@ import java.util.Queue;
 public class ERP {
     Queue<ProductionOrder> productionOrderQueue = new LinkedList<>();
 
-    public boolean addOrder(){
+    public boolean addOrder(int amount, float productType, Date earliestDeliveryDate, Date latestDeliveryDate, int priority){
+        ProductionOrder order = new ProductionOrder(amount, productType, earliestDeliveryDate, latestDeliveryDate, priority);
+        return productionOrderQueue.add(order);
 
-    return true;
     }
 
 }
