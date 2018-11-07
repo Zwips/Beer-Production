d3cd61571d59001ecfeec782a5c438f71138c167
Maltesj <malte_sj@hotmail.com>
2018-11-07 18:50:26 +0100

    added subscribstion til maskinen når den oprettes, satte subscribstions til at gøre noget

--- Semester Project 3/src/main/logic/erp/ProcessingPlant.java
+++ Semester Project 3/src/main/logic/erp/ProcessingPlant.java
@@ -4,15 +4,21 @@ import Acquantiance.IMesMachine;
 
 import java.util.HashMap;
 
+
 public class ProcessingPlant {
     private HashMap<String, IMesMachine> machines;
     private String plantID;
 
+    /**
+     * processing plants containing a Map with Machines
+     * @param plantID the String identifier for the plant
+     */
     ProcessingPlant(String plantID)
     {
         this.plantID = plantID;
         machines = new HashMap<>();
     }
+
 
     boolean addMachine(String machineName, String IPAddress, String userID, String password){
         IMesMachine machine = ERPOutFacade.getInstance().createMachine(machineName, IPAddress, userID, password);
@@ -24,5 +30,15 @@ public class ProcessingPlant {
         {
             return false;
         }
+    }
+
+    boolean checkForMachine(String machineName)
+    {
+        return machines.containsKey(machineName);
+    }
+
+    boolean removeMachine(String machineName)
+    {
+        return machines.remove(machineName, machines.get(machineName));
     }
 }
