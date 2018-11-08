c6686928275ff300f34898c6f25b039613b6daa9
ADoessing <32567122+ADoessing@users.noreply.github.com>
2018-11-08 10:12:21 +0100

    I did do nothing

--- Semester Project 3/src/main/communication/machineConnection/Test.java
+++ Semester Project 3/src/main/communication/machineConnection/Test.java
@@ -19,7 +19,7 @@ public class Test {
     static MachineSpecifications specs = new MachineSpecifications();
 
     private static MachineConnection connection = new MachineConnection("192.168.1.2", "sdu","1234");
-    //private static MachineConnection connection = new MachineConnection("127.0.0.1:4840", "sdu","1234");
+    //private static MachineConnection connection = new MachineConnection("10.112.254.165", "sdu","1234");
 
     public Test() throws FileNotFoundException {
     }
