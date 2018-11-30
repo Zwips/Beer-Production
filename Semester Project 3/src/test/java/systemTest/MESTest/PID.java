8ce36c5a7654bff9c982a4bf0c757345a7867d5c
Maltesj <malte_sj@hotmail.com>
2018-11-30 15:22:22 +0100

    feature for PID

--- Semester Project 3/src/test/java/systemTest/MESTest/PID.java
+++ Semester Project 3/src/test/java/systemTest/MESTest/PID.java
@@ -1,10 +1,15 @@
 package systemTest.MESTest;
 
+import acquantiance.ProductTypeEnum;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import logic.mes.MESOutFacade;
+import org.junit.Assert;
 import systemTest.ERPLevelInitializer;
+
+import static org.junit.Assert.assertEquals;
 
 public class PID {
     @Given("^the system is initialized, atleast at MES level$")
@@ -14,18 +19,26 @@ public class PID {
 
     @Given("^All types but \"([^\"]*)\" is full capacity$")
     public void allTypesButIsFullCapacity(String arg0) throws Throwable {
+        ProductTypeEnum type = ProductTypeEnum.get(arg0);
+        for (ProductTypeEnum value : ProductTypeEnum.values()) {
+            MESOutFacade.getInstance().updateStorageCurrentAmount(100,"theplant",value);
+            MESOutFacade.getInstance().updateStorageTargetAmount(100,"theplant",value);
+        }
+        MESOutFacade.getInstance().updateStorageCurrentAmount(0,"theplant",type);
 
     }
 
     @When("^the PID is queried for next PIDOrder$")
     public void thePIDIsQueriedForNextPIDOrder() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
+
+        //TODO her skal i selv skrive ind hvilke kald PID tager imod for at få det rigtige resultat i næste Then skridtet
         throw new PendingException();
     }
 
     @Then("^the product type for the PIDOrder is \"([^\"]*)\"$")
     public void theProductTypeForThePIDOrderIs(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        ProductTypeEnum type = ProductTypeEnum.get(arg0);
+        //TODO assertEquals(type, pidOrder.getType); eller noget andet i den stil
     }
 }
