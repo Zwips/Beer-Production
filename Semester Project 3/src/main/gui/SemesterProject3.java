d4991a81ca3be21fd68de3280edcfc509b48c7c2
michaeltrier1 <32192126+michaeltrier1@users.noreply.github.com>
2018-12-04 10:19:04 +0100

    OEE til GUI

--- Semester Project 3/src/main/gui/SemesterProject3.java
+++ Semester Project 3/src/main/gui/SemesterProject3.java
@@ -22,7 +22,7 @@ public class SemesterProject3 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
